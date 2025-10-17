8854f167b64dbf4e551c52ba8164af04c7b00c2f
tavishib <tavishi.connect@gmail.com>
2025-10-17 13:33:04 -0700

    Revert "Update module-info.java"
    This reverts commit 092e8e0d14319a9b3a0879a507302b00612cb1c4.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module cs151.application {
+    requires transitive javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
-    opens cs151.application to javafx.fxml, javafx.base;
+    requires com.dlsc.formsfx;
+
+    opens cs151.application to javafx.fxml;
     opens cs151.ui to javafx.fxml;
 
     exports cs151.application;
