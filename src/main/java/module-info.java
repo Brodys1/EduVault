092e8e0d14319a9b3a0879a507302b00612cb1c4
xuanphuong2901 <phuongn2901@gmail.com>
2025-10-17 11:57:40 -0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,8 @@
 module cs151.application {
-    requires transitive javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
-    requires transitive javafx.graphics;
 
-    requires com.dlsc.formsfx;
-
-    opens cs151.application to javafx.fxml;
+    opens cs151.application to javafx.fxml, javafx.base;
     opens cs151.ui to javafx.fxml;
 
     exports cs151.application;
